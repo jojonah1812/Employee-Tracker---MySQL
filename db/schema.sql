f3a886da8e55d131e0d7195c171cb00d2e2ef0cf
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-22 19:35:42 -0700

    Added INSERT INTO's in schema.sql.

--- db/schema.sql
+++ db/schema.sql
@@ -29,3 +29,14 @@ CREATE TABLE employees (
     manager_id INTEGER(9) NOT NULL,
     PRIMARY KEY(id)
 ); 
+
+
+-- New Empoyees
+INSERT INTO departments (name)
+VALUES ("Marketing");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Peacekeeper", "48000", "1");
+
+INSERT INTO employees (first-name, last-name, role_id, manager_id)
+VALUES ("BA", "Baracus", "911", "2");
