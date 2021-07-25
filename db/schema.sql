4ed069a5af763fcff0583913eeee61e728bc612c
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-25 15:47:25 -0700

    Added seeds.sql and remainder of routes.

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,21 @@
 DROP DATABASE IF EXISTS employee_DB;
-
 CREATE DATABASE employee_DB;
-
 USE employee_DB;
 
--- // Create Depts. table //
 CREATE TABLE departments (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 ); 
 
--- // Create Role table //
 CREATE TABLE roles (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30), NOT NULL,
-    salary DECIMAL (9,2)NOT NULL,
+    title VARCHAR(30) NOT NULL, 
+    salary VARCHAR (30) NOT NULL,
     department_id INTEGER(9) NOT NULL,
     PRIMARY KEY (id)
 ); 
 
--- // Create Employee table //
 CREATE TABLE employees (
     id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
@@ -29,14 +24,3 @@ CREATE TABLE employees (
     manager_id INTEGER(9) NOT NULL,
     PRIMARY KEY(id)
 ); 
-
-
--- New Empoyees
-INSERT INTO departments (name)
-VALUES ("Marketing");
-
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Peacekeeper", "48000", "1");
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("BA", "Baracus", "911", "2");
--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const connection = mysql.createConnection({
 const employeeOpt = () => {
   inquirer
     .prompt({
-      name: "listoption",
+      name: "A-Team",
       type: "list",
       choices: [
-        "add department",
+        "Add Department",
         "add role",
         "add employee",
         "view departments",
@@ -28,7 +28,7 @@ const employeeOpt = () => {
 
     .then((answer) => {
       switch (answer.listoption) {
-        case "add department":
+        case "Add Department":
           return addDept();
         case "add role":
           return addRole();
@@ -69,24 +69,124 @@ const addDept = () => {
     });
 };
 ////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const addRole = () => {
+  inquirer
+    .prompt([
+      {
+        name: "newRole",
+        type: "input",
+        message: "Enter new role title",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "Enter Salary",
+      },
+      {
+        name: "deptid",
+        type: "input",
+        message: "What department ID does this employee belong to? ",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO roles SET ?",
+        {
+          title: answer.newRole,
+          salary: answer.salary,
+          department_id: answer.deptid,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("Your role was created successfully!");
+          employeeOpt();
+        }
+      );
+    });
+};
 ///////////////////////////;
+
+const addEmp = () => {
+  inquirer
+    .prompt([
+      {
+        name: "firstname",
+        type: "input",
+        message: "Enter employee first name",
+      },
+      {
+        name: "lastname",
+        type: "input",
+        message: "Enter employee last name",
+      },
+      {
+        name: "role",
+        type: "input",
+        message: "What is the role id?",
+      },
+      {
+        name: "manid",
+        type: "input",
+        message: " What is the manager id",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO employees SET ?",
+        {
+          first_name: answer.firstname,
+          last_name: answer.lastname,
+          role_id: answer.role,
+          manager_id: answer.manid,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("Employee added successfully!");
+          employeeOpt();
+        }
+      );
+    });
+};
+
+//viewing department will make a connection query and show results from db
+const viewDepts = () => {
+  console.log("Pulling up department info...\n");
+  connection.query("SELECT * FROM departments", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    employeeOpt();
+  });
+};
+//do the console table
+
+//viewing employees will make a connection query and show results from db
+const viewEmps = () => {
+  console.log("Pulling up employees...\n");
+  connection.query("SELECT * FROM employees", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    employeeOpt();
+  });
+};
+
+//viewing roles will make a connection query and show results from db
+const viewRoles = () => {
+  console.log("Pulling up roles...\n");
+  connection.query("SELECT * FROM roles", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    employeeOpt();
+  });
+};
+
+/////////////////
+const exit = () => {
+  process.exit();
+};
+
+employeeOpt();
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log(`connected as id ${connection.threadId}`);
-  connection.end();
+  console.log(``);
 });
